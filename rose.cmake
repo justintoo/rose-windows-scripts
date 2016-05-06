aac0a6b524849ccba057b47885fc17905afca6ce
Too <too1@llnl.gov>
2016-05-06 10:24:03 +0100

    (WINDOWS-11) ROSE on Windows 7

--- rose.cmake
+++ rose.cmake
@@ -1,19 +1,18 @@
 CMAKE_MINIMUM_REQUIRED(VERSION 3.1)
 
-set(CTEST_PROJECT_NAME "ROSE")
-set(CTEST_SOURCE_DIRECTORY "C:/jenkins/workspace/104-rose-minimum_config-make_all-edg-cmake-windows/rose")
-set(CTEST_BINARY_DIRECTORY "C:/jenkins/workspace/104-rose-minimum_config-make_all-edg-cmake-windows/ROSE_BIN")
-set(CTEST_CMAKE_GENERATOR "Visual Studio 14 2015 Win64")
+set(CTEST_PROJECT_NAME "rose")
+set(CTEST_SOURCE_DIRECTORY "C:/cygwin64/home/too1/rose")
+set(CTEST_BINARY_DIRECTORY "C:/cygwin64/home/too1/compilation")
+set(CTEST_CMAKE_GENERATOR "Visual Studio 14 2015")
 set(CTEST_CONFIGURATION_TYPE "Release")
 set(CTEST_BUILD_CONFIGURATION "Release")
-set(CTEST_UPDATE_COMMAND "C:/Program Files (x86)/Git/bin/git.exe")
 
 
 ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 
 file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" "
-  BOOST_ROOT:PATH=C:/local/boost/1_59_0
-  Boost_LIBRARY_DIR:PATH=C:/local/boost/1_59_0/lib64-msvc-14.0
+  BOOST_ROOT:PATH=C:/Boost
+  Boost_LIBRARY_DIR:PATH=C:/Boost/lib
   Boost_USE_MULTITHREADED:BOOL=ON
   enable-cuda:BOOL=OFF
   enable-fortran:BOOL=OFF
@@ -30,4 +29,4 @@ ctest_configure(BUILD "${CTEST_BINARY_DIRECTORY}")
 ctest_read_custom_files("${CTEST_BINARY_DIRECTORY}")
 
 ctest_build(BUILD "${CTEST_BINARY_DIRECTORY}")
-ctest_test(BUILD "${CTEST_BINARY_DIRECTORY}" INCLUDE policies)
+#ctest_test(BUILD "${CTEST_BINARY_DIRECTORY}" INCLUDE policies)
--- rose.sh
+++ rose.sh
@@ -1,5 +1,6 @@
-LOG="C:\jenkins\workspace\104-rose-minimum_config-make_all-edg-cmake-windows\ROSE_BIN\rose.log"
-"C:\Users\hudson-rose\development\opt\cmake\3.3.2\bin\ctest.exe" -S "C:\jenkins\jenkins-build-scripts\integration\104-rose-minimum_config-make_all-edg-cmake-windows\rose.cmake" -VV -O "$LOG" || exit 1
+LOG="C:\cygwin64\home\too1\compilation\rose.log"
+
+ctest.exe -S "C:\cygwin64\home\too1\compilation\rose-windows-scripts\rose.cmake" -VV -O "$LOG" || exit 1
 
 if grep -q "Error in read script" "$LOG"; then
   exit 1
